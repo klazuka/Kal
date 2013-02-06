4e61601bb2cb781142c1f41589e47867f2970a9a
Peyman Oreizy <peymano@dynamicvariable.com>
2013-02-06 14:54:16 -0800

    Fix memory leak in KalGridView

--- src/KalGridView.m
+++ src/KalGridView.m
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2009 Keith Lazuka
  * License: http://www.opensource.org/licenses/mit-license.html
  */
@@ -33,7 +33,7 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
 
 - (id)initWithFrame:(CGRect)frame logic:(KalLogic *)theLogic delegate:(id<KalViewDelegate>)theDelegate
 {
-  // MobileCal uses 46px wide tiles, with a 2px inner stroke 
+  // MobileCal uses 46px wide tiles, with a 2px inner stroke
   // along the top and right edges. Since there are 7 columns,
   // the width needs to be 46*7 (322px). But the iPhone's screen
   // is only 320px wide, so we need to make the
@@ -42,12 +42,12 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
   // will be clipped off the screen, but that's fine because
   // MobileCal does the same thing.
   frame.size.width = 7 * kTileSize.width;
-  
+
   if (self = [super initWithFrame:frame]) {
     self.clipsToBounds = YES;
     logic = [theLogic retain];
     delegate = theDelegate;
-    
+
     CGRect monthRect = CGRectMake(0.f, 0.f, frame.size.width, frame.size.height);
     frontMonthView = [[KalMonthView alloc] initWithFrame:monthRect];
     backMonthView = [[KalMonthView alloc] initWithFrame:monthRect];
@@ -82,6 +82,7 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
 {
   if (highlightedTile != tile) {
     highlightedTile.highlighted = NO;
+    [highlightedTile release];
     highlightedTile = [tile retain];
     tile.highlighted = YES;
     [tile setNeedsDisplay];
@@ -92,6 +93,7 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
 {
   if (selectedTile != tile) {
     selectedTile.selected = NO;
+    [selectedTile release];
     selectedTile = [tile retain];
     tile.selected = YES;
     [delegate didSelectDate:tile.date];
@@ -103,10 +105,10 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
   UITouch *touch = [touches anyObject];
   CGPoint location = [touch locationInView:self];
   UIView *hitView = [self hitTest:location withEvent:event];
-  
+
   if (!hitView)
     return;
-  
+
   if ([hitView isKindOfClass:[KalTileView class]]) {
     KalTileView *tile = (KalTileView*)hitView;
     if (tile.belongsToAdjacentMonth) {
@@ -133,7 +135,7 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
   UITouch *touch = [touches anyObject];
   CGPoint location = [touch locationInView:self];
   UIView *hitView = [self hitTest:location withEvent:event];
-  
+
   if ([hitView isKindOfClass:[KalTileView class]]) {
     KalTileView *tile = (KalTileView*)hitView;
     if (tile.belongsToAdjacentMonth) {
@@ -156,7 +158,7 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
 - (void)swapMonthsAndSlide:(int)direction keepOneRow:(BOOL)keepOneRow
 {
   backMonthView.hidden = NO;
-  
+
   // set initial positions before the slide
   if (direction == SLIDE_UP) {
     backMonthView.top = keepOneRow
@@ -169,22 +171,22 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
   } else {
     backMonthView.top = 0.f;
   }
-  
+
   // trigger the slide animation
   [UIView beginAnimations:kSlideAnimationId context:NULL]; {
     [UIView setAnimationsEnabled:direction!=SLIDE_NONE];
     [UIView setAnimationDuration:0.5];
     [UIView setAnimationDelegate:self];
     [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
-    
+
     frontMonthView.top = -backMonthView.top;
     backMonthView.top = 0.f;
 
     frontMonthView.alpha = 0.f;
     backMonthView.alpha = 1.f;
-    
+
     self.height = backMonthView.height;
-    
+
     [self swapMonthViews];
   } [UIView commitAnimations];
  [UIView setAnimationsEnabled:YES];
@@ -193,21 +195,21 @@ static NSString *kSlideAnimationId = @"KalSwitchMonths";
 - (void)slide:(int)direction
 {
   transitioning = YES;
-  
+
   [backMonthView showDates:logic.daysInSelectedMonth
       leadingAdjacentDates:logic.daysInFinalWeekOfPreviousMonth
      trailingAdjacentDates:logic.daysInFirstWeekOfFollowingMonth];
-  
+
   // At this point, the calendar logic has already been advanced or retreated to the
-  // following/previous month, so in order to determine whether there are 
+  // following/previous month, so in order to determine whether there are
   // any cells to keep, we need to check for a partial week in the month
   // that is sliding offscreen.
-  
+
   BOOL keepOneRow = (direction == SLIDE_UP && [logic.daysInFinalWeekOfPreviousMonth count] > 0)
                  || (direction == SLIDE_DOWN && [logic.daysInFirstWeekOfFollowingMonth count] > 0);
-  
+
   [self swapMonthsAndSlide:direction keepOneRow:keepOneRow];
-  
+
   self.selectedTile = [frontMonthView firstTileOfMonth];
 }
 
