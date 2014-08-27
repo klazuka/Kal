65ecb41a25226a8a730714586e48c5ebe006a3aa
ArtemShiyanov <arrtem.shyianov@gmail.com>
2014-08-27 17:00:13 +0300

    Update KalViewController.h

--- src/KalViewController.h
+++ src/KalViewController.h
@@ -24,7 +24,7 @@
 {
   KalLogic *logic;
   UITableView *tableView;
-  id <UITableViewDelegate> delegate;
+  id /*<UITableViewDelegate>*/ delegate;
   id <KalDataSource> dataSource;
   NSDate *initialDate;                    // The date that the calendar was initialized with *or* the currently selected date when the view hierarchy was torn down in order to satisfy a low memory warning.
   NSDate *selectedDate;                   // I cache the selected date because when we respond to a memory warning, we cannot rely on the view hierarchy still being alive, and thus we cannot always derive the selected date from KalView's selectedDate property.
@@ -37,5 +37,5 @@
 - (id)initWithSelectedDate:(NSDate *)selectedDate;  // designated initializer. When the calendar is first displayed to the user, the month that contains 'selectedDate' will be shown and the corresponding tile for 'selectedDate' will be automatically selected.
 - (void)reloadData;                                 // If you change the KalDataSource after the KalViewController has already been displayed to the user, you must call this method in order for the view to reflect the new data.
 - (void)showAndSelectDate:(NSDate *)date;           // Updates the state of the calendar to display the specified date's month and selects the tile for that date.
-
+- (void)didSelectDate:(KalDate *)date;
 @end
